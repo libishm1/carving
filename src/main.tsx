717b8ec3009daae4b6fe3bc16313feebe7220681
Godofscript <38063326+Godofscript@users.noreply.github.com>
2026-06-10 20:45:56 +0300

    fix: remove unused StrictMode import to fix tsc build failure

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 /// <reference types="vite-plugin-pwa/client" />
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
